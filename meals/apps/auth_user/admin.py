d75aa584b329aea4aeb2d72ace9a9d2518da05c9
baliubakh <74013378+baliubakh@users.noreply.github.com>
2023-01-31 20:58:21 +0200

    Revert "Add registration and CustomUser model"

--- meals/apps/auth_user/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-from apps.auth_user.models import CustomUser
-
-
-admin.site.register(CustomUser)
--- meals/apps/auth_user/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from apps.auth_user.views import UserCreateView
-
-urlpatterns = [
-    path('register/', UserCreateView.as_view()),
-]
