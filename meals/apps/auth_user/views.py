700e58e0af5e57af05b2b64ccf0629d747a96d6c
Michael <leroygorn@gmail.com>
2023-02-01 11:30:11 +0200

    Add password validation for CustomUser

--- meals/apps/auth_user/views.py
+++ meals/apps/auth_user/views.py
@@ -1,6 +1,10 @@
-from rest_framework import generics
-from rest_framework.permissions import AllowAny
-from apps.auth_user.serializers import UserSerializer
+from rest_framework import generics, status
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from apps.auth_user.serializers import UserSerializer, CustomObtainTokenSerializer
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 
 class UserCreateView(generics.CreateAPIView):
@@ -8,3 +12,25 @@ class UserCreateView(generics.CreateAPIView):
         AllowAny,
     )
     serializer_class = UserSerializer
+
+
+class UserLoginView(TokenObtainPairView):
+    permission_classes = (
+        AllowAny,
+    )
+    serializer_class = CustomObtainTokenSerializer
+
+
+class UserLogoutView(APIView):
+    permission_classes = (
+        IsAuthenticated,
+    )
+
+    def post(self, request):
+        try:
+            refresh_token = request.data['refresh']
+            refresh_token = RefreshToken(refresh_token)
+            refresh_token.blacklist()
+            return Response("Successful Logout", status=status.HTTP_205_RESET_CONTENT)
+        except Exception as exc:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
