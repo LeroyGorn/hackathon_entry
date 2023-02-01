393fb1e6a18802736b279d236fdd86332c3494bf
Roman Semenenko <semenenkoroma01@gmail.com>
2023-02-01 19:43:58 +0200

    Add ProductListAPIView

--- meals/apps/products/views.py
+++ meals/apps/products/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.pagination import LimitOffsetPagination
+from rest_framework.permissions import AllowAny
 
-# Create your views here.
+from apps.products.models import Product
+from apps.products.serializers import ProductSerializer
+
+
+class ProductListAPIView(generics.ListAPIView):
+    permission_classes = (
+        AllowAny,
+    )
+    pagination_class = LimitOffsetPagination
+    serializer_class = ProductSerializer
+
+    def get_queryset(self):
+        return Product.objects.all()
