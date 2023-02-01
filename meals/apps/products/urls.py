c215bfdcdcbe0824dc9c07ea547bcea6a44e39ee
Michael <leroygorn@gmail.com>
2023-02-01 20:14:44 +0200

    Added user products view

--- meals/apps/products/urls.py
+++ meals/apps/products/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from apps.products.views import ProductListAPIView
+from apps.products.views import ProductListAPIView, UserProductsListAPIView
 
 urlpatterns = [
     path('', ProductListAPIView.as_view()),
+    path('user/<int:user_id>/', UserProductsListAPIView.as_view()),
 ]
