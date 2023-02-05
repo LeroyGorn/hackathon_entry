84a00878c463cde01ec1c51f75d66fb7955880b8
Michael <leroygorn@gmail.com>
2023-02-05 19:48:45 +0200

    Added refresh token url

--- meals/apps/auth_user/urls.py
+++ meals/apps/auth_user/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView
 
 from apps.auth_user.views import UserCreateView, UserLoginView, UserLogoutView
 
 urlpatterns = [
+    path("refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path('register/', UserCreateView.as_view()),
     path('login/', UserLoginView.as_view(), name="user_login"),
     path('logout/', UserLogoutView.as_view(), name="user_logout"),
