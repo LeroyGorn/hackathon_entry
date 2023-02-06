274b030f9195b8d88c7e3bc5c60acd220e2bbe90
Michael <leroygorn@gmail.com>
2023-02-07 00:23:47 +0200

    Additional cors settings

--- meals/meals/settings.py
+++ meals/meals/settings.py
@@ -171,6 +171,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+CORS_ALLOW_ALL_ORIGINS = True
+
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
     'http://ec2-44-203-241-87.compute-1.amazonaws.com',
