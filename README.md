6d0b4dad7df052794b2a5a423d5959111b68f9d4
Michael <leroygorn@gmail.com>
2023-02-07 16:24:18 +0200

    Added env file and readme

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # hackathon_entry
+
+Hey there, it is Los Pollos Hermanos Hackathon 2023 project.
+
+First of all you can get our web site by the link:
+https://pollos-entry.herokuapp.com/api/auth/login/
+Firstly you need to register your account to get access to full functionality of our project.
+Our website offers you to:
+
+1) View lots of dishes receipts;
+
+2) Take a look and buy different products from a wide range of choice;
+
+3) Check what dishes you can cook with products you have !
+
+4) Search for a dish by product, category or name.
+
+And more something interesting !
+
+Shortly about technology stack our project created with:
+ 
+ - Django REST Framework, Python3
+ - React, Typescript, Formik, Redux toolkit 
--- meals/meals/settings.py
+++ meals/meals/settings.py
@@ -12,7 +12,9 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 import os
 from datetime import timedelta
 from pathlib import Path
+from dotenv import load_dotenv
 
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,12 +23,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&q@sc!gmhx*ki)nhfj668xm8fx@_&h&go9p#9ib3jt5p-4-yw4'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = [
+    'pollos-entry.herokuapp.com',
     '44.203.241.87',
     'ec2-44-203-241-87.compute-1.amazonaws.com',
     'localhost',
